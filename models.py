f604268ae7e6e5b1c5f81a89a6ff251715351208
Your Name <you@example.com>
2023-12-01 20:09:52 -0800

    First Commit

--- models.py
+++ models.py
@@ -1,1 +1,36 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMG_URL = "https://tinyurl.com/demo-cupcake"
+
+
+class Cupcake(db.Model):
+    """cupcakes"""
+
+
+__tablename__ = "cupcakes"
+
+id = db.Column(
+    db.Integer,
+    primary_key=True)
+
+flavor = db.Column(
+    db.Text,
+    nullable=False)
+
+size = db.Column(
+    db.Text,
+    nullable=False)
+
+rating = db.Column(
+    db.Float,
+    nullable=False)
+
+image_url = db.Column(
+    db.Text,
+    nullable=False,
+    default=DEFAULT_IMG_URL
+)
